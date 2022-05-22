2299df9112b5c92abcdbabd9fd9dbc1431f382ac
weixiang0470 <s111010550@student.nqu.edu.tw>
2022-05-22 18:02:00 +0800

    HW7

---
+++ Homework/HW7/Regexp.js
@@ -0,0 +1,22 @@
+let text = "Name:Xiang Age:22 Work:Student Email:s111010550@student.nqu.edu.tw test:s1110@s";
+
+
+let re1 = /[0-9]+/g ;
+let re2 = /[a-z]+/g ;
+let re3 = /:([0-9a-zA-Z])+/g ;
+let re4 = /:([0-9a-zA-Z])+/ ;
+let re5 = /:([0-9a-zA-Z]+)/ ;
+let re6 = /:\d+/g ;
+let re7 = /:\D+/g ;
+let re8 = /\w+@[\w\.]+/g ;
+let re9 = /\w+@[a-z]+\.[a-z.]+/g ; 
+
+console.log("/[0-9]+/g ==> "+text.match(re1));
+console.log("/[a-z]+/g ==> "+text.match(re2));
+console.log("/:([0-9a-zA-Z])+/g ==> "+text.match(re3));
+console.log("/:([0-9a-zA-Z])+/ ==> "+text.match(re4));
+console.log("/:([0-9a-zA-Z]+)/ ==> "+text.match(re5));
+console.log("/:\d+/g ==> "+text.match(re6));
+console.log("/:\D+/g ==> "+text.match(re7));
+console.log("/\w+@[\w\.]+/g ==> "+text.match(re8));
+console.log("\w+@[a-z]+\.[a-z.]+/g ==> "+text.match(re9));
